2828a2466957aa04dad9c7cb70f7a909721fab28
Chris James <drummerboof@gmail.com>
2013-08-04 21:01:35 +0100

    Comments for the LayoutView class

--- lib/SlimStatic/View/LayoutView.php
+++ lib/SlimStatic/View/LayoutView.php
@@ -2,6 +2,12 @@
 
 namespace SlimStatic\View;
 
+/**
+ * Class LayoutView
+ * @package SlimStatic\View
+ *
+ * LayoutView provides a means to use layouts to wrap individual template output.
+ */
 class LayoutView extends \Slim\View
 {
 
@@ -12,24 +18,42 @@ class LayoutView extends \Slim\View
 
     protected $layout;
 
+    /**
+     * @param null|string $layout
+     */
     public function __construct($layout = null)
     {
         $this->setLayout($layout);
     }
 
+    /**
+     * Set the layout file. The contents of this file will be used to wrap the output returned
+     * from calling render.
+     *
+     * @param string $layout
+     */
     public function setLayout($layout)
     {
         $this->layout = self::$LAYOUT . $layout;
     }
 
+    /**
+     * Render a template file. If a layout has been given then the result of rendering this
+     * template is set as the $_content variable within the layout template which is then
+     * rendered and returned.
+     *
+     * @param string $template
+     * @param null|string $prefix
+     * @return string
+     */
     public function render($template, $prefix = null)
     {
         if ($this->hasLayout())
         {
             $content = parent::render(($prefix ? $prefix : self::$PAGE) . $template);
-            $this->setData(array_merge($this->getData(), array(
+            $this->appendData(array(
                 '_content' => $content
-            )));
+            ));
             return parent::render($this->layout);
         }
         else
@@ -38,6 +62,14 @@ class LayoutView extends \Slim\View
         }
     }
 
+    /**
+     * Render a template bypassing any of the layout logic.
+     * Used for rendering templates within a template
+     *
+     * @param string $template
+     * @param array $data
+     * @return string
+     */
     public function partial($template, $data = array())
     {
         if (!empty($data)) {
@@ -46,6 +78,11 @@ class LayoutView extends \Slim\View
         return parent::render(self::$PARTIAL . $template);
     }
 
+    /**
+     * Return true if a layout has been set on the view
+     * 
+     * @return bool
+     */
     public function hasLayout()
     {
         return $this->layout !== null;
